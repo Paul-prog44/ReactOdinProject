6e3fb23b980c226d99e5ddb739eada97bbc65196
Paul-prog44 <saintouenfr@hotmail.com>
2024-06-04 16:19:44 +0200

    Add general informations

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import Title from './components/title'
+import GeneralInformation from './components/generalInformation'
 
 function App() {
   return (
     <>
     <Title/>
     <div className='page'>
-      <div className='parts'></div>
+      <div className='parts'>
+        <GeneralInformation/>
+      </div>
       <div className='parts'></div>
     </div>
     </>
---
+++ src/components/generalInformation.jsx
@@ -0,0 +1,14 @@
+function GeneralInformation() {
+    return (
+        <>
+            <h2>Informations générales</h2>
+            <p>Nom :<input name="name"/></p>
+            <p>Prénom :<input name="lastname"/></p>
+            <p>Numéro de téléphone :<input name="phone"/></p>
+            <p>Adresse :<input name="address"/></p>
+            <p>Adresse éléctronique :<input name="emailAddress"/></p>
+        </>
+    )
+}
+
+export default GeneralInformation
