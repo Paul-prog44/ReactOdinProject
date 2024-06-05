08693ebd8b046fe674025f8a3e3c1ba96dba11f9
Paul-prog44 <saintouenfr@hotmail.com>
2024-06-05 13:37:38 +0200

    Add state to practicalExperience

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,12 @@
-function Input({label, name}) {
+function Input({name, label, type, value, onInputChange }) {
+
+    const handleChange = (event) => {
+        // Appelle la fonction passée en props pour mettre à jour l'état du parent
+        onInputChange(name, event.target.value);
+      };
+
     return (
-        <label>{label}: <input name ={name} id={name}></input></label>
+        <label>{label}: <input name ={name} id={name} value={value} type={type} onChange={handleChange}></input></label>
     )
 }
 
--- src/components/PracticalExperience.jsx
+++ src/components/PracticalExperience.jsx
@@ -1,14 +1,31 @@
+import { useState } from "react";
 import Input from "./Input";
 
 function PracticalExperience() {
+    const [workExperience, setWorkExperience] = useState({
+        positionTitle:"",
+        company:"",
+        tasks:"",
+        startYear:"",
+        endYear:""
+    })
+
+    //fonction de gestion de changement
+    const handleChange = (field, value) => {
+        setWorkExperience(prevState => ({
+            ...prevState,
+            [field]:value
+        }))
+    }
+
     return (
         <>
             <h2>Expérience professionnelle</h2>
-            <Input name="positionTitle" label="Poste"/>
-            <Input name="company" label="Entreprise"/>
-            <Input name="tasks" label="Taches principales"/>
-            <Input name="startYear" label="Date de début"/>
-            <Input name="endYear" label="Date de fin"/>
+            <Input name="positionTitle" label="Poste" type="text" value={workExperience.positionTitle} onInputChange={handleChange}/>
+            <Input name="company" label="Entreprise" type="text" value={workExperience.company} onInputChange={handleChange}/>
+            <Input name="tasks" label="Taches principales" type="text" value={workExperience.tasks} onInputChange={handleChange}/>
+            <Input name="startYear" label="Date de début" type="number" value={workExperience.startYear} onInputChange={handleChange}/>
+            <Input name="endYear" label="Date de fin" type="number" value={workExperience.endYear} onInputChange={handleChange}/>
         </>
     )
 }
